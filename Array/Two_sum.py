2503bef28cc65531bd7c9ab32ed842277267f1d2
Lidia-Kibret <lidiakibret@Lidias-MacBook-Air.local>
2026-03-15 01:08:57 +0300

    Solved Two Sum - LeetCode

---
+++ Array/Two_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = {}
+
+        for i in range(len(nums)):
+            count = target - nums[i]
+
+            if count in result:
+                return [result[count], i]
+
+            result[nums[i]] = i
